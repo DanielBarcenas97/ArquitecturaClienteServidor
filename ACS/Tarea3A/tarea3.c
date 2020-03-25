6d4f81500f572302a2ad19e6d8669c040ac61522
danielbarcenas <barcenas.proteco@gmail.com>
2020-03-24 18:49:56 -0600

    Tarea3

---
+++ ACS/Tarea3A/tarea3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+	printf("Ortega Moreno Omar Orlando \n");
+	printf("1: el id del proceso es %d/n", getpid());
+	sleep(60);
+	printf("2: Me despierto y termino \n");
+}
+
+
+
